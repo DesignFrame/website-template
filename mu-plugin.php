a795373ba5de8c63a5079155b084f728faa3387c
Alex Standiford <a@alexstandiford.com>
2021-04-29 17:31:03 -0700

    initial commit

---
+++ mu-plugin.php
@@ -0,0 +1,8 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Load Underpin
+require_once( plugin_dir_path( __FILE__ ) . 'vendor/alexstandiford/underpin/Underpin.php' );
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Website Repository
+
+This is a website repository. It houses all of the files and configuration necessary to create a duplicate of a site.
